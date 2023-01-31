0f9a628402254bd7d9de3c20b7d150940a9401e2
Jezebel1990 <jezebelguedes@gmail.com>
2023-01-31 12:50:51 -0300

    add data

---
+++ src/Datas.js
@@ -0,0 +1,27 @@
+export const news = [
+
+
+{
+
+    title: "Noticia 1",
+    text: "	Lorem ipsum himenaeos bibendum dolor nisl tellus adipiscing tempus inceptos lorem sollicitudin, tellus per nullam vestibulum ligula pellentesque senectus est fringilla. vel semper duis pellentesque libero lectus eleifend, eget quam cursus pretium habitant, nibh ac odio leo senectus. rhoncus platea pulvinar molestie elit ac nunc ipsum primis mi, aptent pharetra faucibus vivamus senectus habitant viverra et vulputate nam, varius semper rhoncus venenatis mattis aliquam sem ut. erat class eget auctor dictumst platea pulvinar enim blandit imperdiet, fames dolor turpis ligula per ac laoreet tristique quisque gravida, vitae potenti varius felis blandit etiam lacus tincidunt.",
+    image: "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comments: 4,
+},
+{
+    title: "Noticia 2",
+    text: "	Lorem ipsum himenaeos bibendum dolor nisl tellus adipiscing tempus inceptos lorem sollicitudin, tellus per nullam vestibulum ligula pellentesque senectus est fringilla. vel semper duis pellentesque libero lectus eleifend, eget quam cursus pretium habitant, nibh ac odio leo senectus. rhoncus platea pulvinar molestie elit ac nunc ipsum primis mi, aptent pharetra faucibus vivamus senectus habitant viverra et vulputate nam, varius semper rhoncus venenatis mattis aliquam sem ut. erat class eget auctor dictumst platea pulvinar enim blandit imperdiet, fames dolor turpis ligula per ac laoreet tristique quisque gravida, vitae potenti varius felis blandit etiam lacus tincidunt.",
+    image: "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comments: 4,
+},
+
+{
+    title: "Noticia 3",
+    text: "	Lorem ipsum himenaeos bibendum dolor nisl tellus adipiscing tempus inceptos lorem sollicitudin, tellus per nullam vestibulum ligula pellentesque senectus est fringilla. vel semper duis pellentesque libero lectus eleifend, eget quam cursus pretium habitant, nibh ac odio leo senectus. rhoncus platea pulvinar molestie elit ac nunc ipsum primis mi, aptent pharetra faucibus vivamus senectus habitant viverra et vulputate nam, varius semper rhoncus venenatis mattis aliquam sem ut. erat class eget auctor dictumst platea pulvinar enim blandit imperdiet, fames dolor turpis ligula per ac laoreet tristique quisque gravida, vitae potenti varius felis blandit etiam lacus tincidunt.",
+    image: "https://www.aberje.com.br/wp-content/uploads/2016/03/lorem-ipsum.jpg",
+    likes: 2,
+    comments: 4,
+}
+]
